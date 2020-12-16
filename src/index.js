637ee96c41814b2b539c9675f581092c688c0ef1
杜康 <banshee1115@163.com>
2020-12-16 16:51:10 +0800

    创建包

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 /*
  * @Author: 杜康
  * @Date: 2020-12-16 16:28:35
- * @LastEditTime: 2020-12-16 16:31:37
+ * @LastEditTime: 2020-12-16 16:43:09
  * @LastEditors: Please set LastEditors
  * @Description: 创建index.js
  * @FilePath: \webpack-learning\src\index.js
  */
+import _ from 'lodash';
 function component () {
   const element = document.createElement('div')
+  // lodash已本地引入
   element.innerHTML = _.join(['Hello', 'webpack'], ' ')
   return element
 }
