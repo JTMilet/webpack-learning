3f8cc24b9dd55aff51d27295038af1f4be333f14
杜康 <banshee1115@163.com>
2020-12-17 14:12:46 +0800

    输出管理：多个输出管理

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 /*
  * @Author: 杜康
  * @Date: 2020-12-16 16:28:35
- * @LastEditTime: 2020-12-17 13:55:32
- * @LastEditors: 杜康 VX：Dk_Ns_Gyy
+ * @LastEditTime: 2020-12-17 14:04:43
+ * @LastEditors: 杜康 VX:Dk_Ns_Gyy
  * @Description: 创建index.js
  * @FilePath: \webpack-learning\src\index.js
  */
 import _ from 'lodash'
+import printMe from './print'
 function component () {
   const element = document.createElement('div')
+  const btn = document.createElement('button')
   // lodash已本地引入
   element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+  btn.innerHTML = 'Click me and check the console!'
+  btn.onclick = printMe
+  element.appendChild(btn)
   return element
 }
 document.body.appendChild(component())
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,19 @@
 /*
  * @Author: 杜康
  * @Date: 2020-12-16 17:02:31
- * @LastEditTime: 2020-12-17 13:54:58
- * @LastEditors: 杜康 VX：Dk_Ns_Gyy
+ * @LastEditTime: 2020-12-17 14:08:00
+ * @LastEditors: 杜康 VX:Dk_Ns_Gyy
  * @Description: 创建webpack.config.js
  * @FilePath: \webpack-learning\webpack.config.js
  */
 const path = require('path')
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    index: './src/index.js',
+    print: './src/print.js',
+  },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   }
 }
