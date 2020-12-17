870b25c1a6b74fc9bbc42cf922efc2566d464fe3
杜康 <banshee1115@163.com>
2020-12-17 10:21:42 +0800

    资产管理 （css, img）

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 /*
  * @Author: 杜康
  * @Date: 2020-12-16 17:02:31
- * @LastEditTime: 2020-12-16 17:25:16
+ * @LastEditTime: 2020-12-17 10:06:38
  * @LastEditors: 杜康 微信：Dk_Ns_Gyy
  * @Description: 创建webpack.config.js
  * @FilePath: \webpack-learning\webpack.config.js
@@ -10,7 +10,19 @@ const path = require('path')
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource'
+      }
+    ]
   }
 }
