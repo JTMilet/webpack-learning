395c18c6815c7f7bcc97cc648322ff3f7bd85d67
杜康 <banshee1115@163.com>
2020-12-17 14:51:12 +0800

    加入dist的html文件生产插件HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,23 @@
 /*
  * @Author: 杜康
  * @Date: 2020-12-16 17:02:31
- * @LastEditTime: 2020-12-17 14:08:00
+ * @LastEditTime: 2020-12-17 14:20:46
  * @LastEditors: 杜康 VX:Dk_Ns_Gyy
  * @Description: 创建webpack.config.js
  * @FilePath: \webpack-learning\webpack.config.js
  */
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
   entry: {
     index: './src/index.js',
     print: './src/print.js',
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+    })
+  ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
