a40d2c250b0f6a7222814e845e6777939d950468
杜康 <banshee1115@163.com>
2020-12-17 11:22:33 +0800

    资产管理 （fonts）

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 /*
  * @Author: 杜康
  * @Date: 2020-12-16 17:02:31
- * @LastEditTime: 2020-12-17 10:06:38
+ * @LastEditTime: 2020-12-17 10:26:06
  * @LastEditors: 杜康 微信：Dk_Ns_Gyy
  * @Description: 创建webpack.config.js
  * @FilePath: \webpack-learning\webpack.config.js
@@ -15,12 +15,19 @@ module.exports = {
   },
   module: {
     rules: [
+      // style
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader']
       },
+      // image
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource'
+      },
+      // fonts
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource'
       }
     ]
