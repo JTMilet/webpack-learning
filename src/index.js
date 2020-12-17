973a5e3e6fdab39a3781b5e59ce28a9c601e7a0d
杜康 <banshee1115@163.com>
2020-12-17 13:35:48 +0800

    加载数据（toml yaml json5）

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: 杜康
  * @Date: 2020-12-16 16:28:35
- * @LastEditTime: 2020-12-17 11:49:06
+ * @LastEditTime: 2020-12-17 13:31:25
  * @LastEditors: 杜康 VX：Dk_Ns_Gyy
  * @Description: 创建index.js
  * @FilePath: \webpack-learning\src\index.js
@@ -11,6 +11,15 @@ import './style.css'
 import Icon from './icon.jpg'
 import Data from './data.xml'
 import Notes from './data.csv'
+import toml from './data.toml'
+import yaml from './data.yaml'
+import json from './data.json5'
+console.log(toml.title)
+console.log(toml.owner.name)
+console.log(yaml.title)
+console.log(yaml.owner.name)
+console.log(json.title)
+console.log(json.owner.name)
 function component () {
   const element = document.createElement('div')
   // lodash已本地引入
