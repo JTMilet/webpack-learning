848223f17033eb34bd78c5b769edfb5d310e65fd
杜康 <banshee1115@163.com>
2020-12-18 09:50:38 +0800

    动态导入打包

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 /*
  * @Author: 杜康
  * @Date: 2020-12-16 16:28:35
- * @LastEditTime: 2020-12-18 09:08:00
+ * @LastEditTime: 2020-12-18 09:48:46
  * @LastEditors: 杜康 VX:Dk_Ns_Gyy
  * @Description: 创建index.js
  * @FilePath: \webpack-learning\src\index.js
  */
-import _ from 'lodash'
-function component () {
-  const element = document.createElement('div')
-  // lodash已本地引入
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+async function getComponent () {
+  const { default: _ } = await import('lodash')
+  element.innerHTML = _.join(['Hello', 'Webpack!'], ' ')
   return element
 }
-document.body.appendChild(component())
+getComponent()
+  .then(component => {
+    document.body.appendChild(component())
+  })
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 /*
  * @Author: 杜康
  * @Date: 2020-12-16 17:02:31
- * @LastEditTime: 2020-12-18 09:26:11
+ * @LastEditTime: 2020-12-18 09:32:46
  * @LastEditors: 杜康 VX:Dk_Ns_Gyy
  * @Description: 创建webpack.config.js
  * @FilePath: \webpack-learning\webpack.config.js
@@ -12,17 +12,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 module.exports = {
   mode: 'development',
   entry: {
-    // index: './src/index.js',
-    // another: './src/another-module.js',
-    index: {
-      import: './src/index.js',
-      dependOn: 'shared'
-    },
-    another: {
-      import: './src/another-module.js',
-      dependOn: 'shared'
-    },
-    shared: 'lodash'
+    index: './src/index.js'
   },
   devtool: 'inline-source-map',
   devServer: {
@@ -40,10 +30,5 @@ module.exports = {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/'
-  },
-  optimization: {
-    splitChunks: {
-      chunks: 'all'
-    }
   }
 }
